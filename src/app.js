a6ab513926a91d3b8b2a4405aa2db033527d656b
Sora Olaya Yovany <ysorao@heon.com.co>
2022-11-08 20:47:59 -0500

    aplicacion de cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import config from './config'
 import infoRoutes from './routes/info.routes'
+const cors =  require('cors')
 
 const app = express();
 
 //settings
 app.set('port', config.port ) 
+app.use(cors())
 
 //middlewares
 app.use(express.json())
